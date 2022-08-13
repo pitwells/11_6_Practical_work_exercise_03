4b4a83243ce856b73cf6fb9ecbcdf1c3addf1214
Дмитрий Богданов <64403847+DmitriyBogdanov@users.noreply.github.com>
2022-08-13 19:16:14 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+std::string qwe;
+std::string ip = "192.168.0.1";
+int octet = 1;
+
+std::string get_address_part (ip, octet); {
+
+    return std::cout << "cd";
+
+}
+
+
+int main() {
+    std::cout << "IP Address Validation" << std::endl << std::endl;
+
+    //get_address_part(ip, octet);
+
+    //check_valid();
+
+    std::cout << "END";
+    return 0;
+}
