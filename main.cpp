96e4b3b9dcfa22e6fe89d75bc4b55b5c13e46d62
Дмитрий Богданов <64403847+DmitriyBogdanov@users.noreply.github.com>
2022-08-14 22:19:21 +0300

    get_address_part - done

--- main.cpp
+++ main.cpp
@@ -2,31 +2,23 @@
 
 std::string qwe;
 std::string ip = "123.456.7.8";
-int octet = 0;
+int octet = 3;
 
 std::string get_address_part (std::string ip, int octet) {
-    std::string qwe;
-    int dot = 0; // добавить переменную которая будет считать номер октета
-    // и условие которое будет останавливать цикл когда он будет равен заданаму.
+    std::string partIp;
+    int dot = 0;
     for (int i = 0; i < ip.length(); i++) {
-        if (ip[i] != '.' && dot == octet) {
-            qwe += ip[i];
-            std::cout << "QWE " << qwe << std::endl;
-        }
-
-        dot += 1;
-        std::cout << "1";
+        if (ip[i] != '.' && dot == octet) partIp += ip[i];
+            else if (ip[i] == '.') dot += 1;
     }
-    std::cout << "DOT " << dot << std::endl;
-    std::cout << "2";
-    return qwe;
+    return partIp;
 }
 
 
 int main() {
     std::cout << "IP Address Validation" << std::endl << std::endl;
 
-    get_address_part(ip, 0);
+    get_address_part(ip, octet);
 
     //check_valid();
 
